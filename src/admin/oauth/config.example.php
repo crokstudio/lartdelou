e0009bf892965bc44d9ed7d60005cd3dd7997b8d
crokstudio <crokstudio@gmail.com>
2026-06-08 15:33:28 +0200

    Revert "adaptation to ovh"
    This reverts commit 4dd5cac57333abb3735e6a909c658c51868074dc.

--- src/admin/oauth/config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'github_client_id' => 'REMPLACER_PAR_LE_CLIENT_ID_GITHUB',
-    'github_client_secret' => 'REMPLACER_PAR_LE_CLIENT_SECRET_GITHUB',
-    'redirect_uri' => 'https://www.lartdelou.be/admin/oauth/callback.php',
-    'allowed_origin' => 'https://www.lartdelou.be',
-];
